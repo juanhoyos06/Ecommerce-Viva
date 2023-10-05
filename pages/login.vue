88b6d0f84d31cbb605c4fe497a99fc1325dc1e9f
juan <jujosal007@gmail.com>
2023-10-05 14:40:02 -0500

    Feat: se agrego sweetalert

--- pages/login.vue
+++ pages/login.vue
@@ -1,8 +1,7 @@
 <template>
   <div>
     <v-sheet rounded="xl" elevation="12" max-width="400" width="90%" class="ma-10 pa-5 mx-auto">
-      <v-btn href="/" variant="text" class="position-absolute top-0 left-2" size="x-large"
-        icon="mdi-arrow-left"></v-btn>
+      <v-btn href="/" variant="text" class="position-absolute top-0 left-2" size="x-large" icon="mdi-arrow-left"></v-btn>
       <center>
         <img src="../assets/img/logo_viva_login.png" height="200" class="ml-20">
       </center>
@@ -11,13 +10,12 @@
         <v-form fast-fail @submit.prevent="login">
           <v-text-field v-model="email" label="Correo" type="email" prepend-icon="mdi-email"></v-text-field>
           <v-text-field v-model="password" type="password" label="Contraseña" prepend-icon="mdi-key"></v-text-field>
-          <a href="#" class="text-body-2 font-weight-regular">Olvide mi contraseña</a>
+          <a href="#" class="text-body-2 font-weight-regular">Olvidé mi contraseña</a>
 
           <v-btn type="submit" color="black" block class="mt-2">Ingresar</v-btn>
-
         </v-form>
         <div class="mt-2">
-          <p class="text-body-2">¿No tiene cuenta?
+          <p class="text-body-2">¿No tienes cuenta?
             <NuxtLink to="/register">
               Registrarse
             </NuxtLink>
@@ -27,26 +25,17 @@
     </v-sheet>
   </div>
 </template>
+
 <script>
 import axios from 'axios';
+import Swal from 'sweetalert2';
 
 export default {
   data() {
     return {
       email: '',
       password: '',
-      visible: false,
       users: [],
-      emailRules: [
-        value => {
-          if (value) return true
-          return 'El campo es obligatorio.'
-        },
-        value => {
-          if (/[^[^@]+@[^@]+\.[a-zA-Z]{2,}$/.test(value)) return true
-          return 'Correo no valido.'
-        }
-      ],
     };
   },
   methods: {
@@ -68,23 +57,29 @@ export default {
 
       if (foundUser) {
         console.log('Inicio de sesión exitoso para el usuario:', foundUser);
+        // Redirige al usuario a la página de inicio
         this.$router.push('/');
       } else {
-        console.error('Credenciales incorrectas. Inicio de sesión fallido.');
+        // Muestra una alerta de SweetAlert2 en caso de credenciales incorrectas
+        Swal.fire({
+          icon: 'error',
+          title: 'Credenciales incorrectas',
+          text: 'Inicio de sesión fallido. Verifica tu correo y contraseña.',
+        });
       }
     },
   },
 };
 definePageMeta({
-  layout: "blank",
+    layout: "blank",
 });
 </script>
+
 <style>
 body {
   width: 100%;
   height: 100%;
   background-image: url(./assets/img/fondo_login.jpg);
   background-size: cover;
-
 }
 </style>
