c6b27fae46dee8ff312f6a9c915714ad9b1acef6
juanhoyos06 <jphoyos0610@gmail.com>
2023-10-13 18:15:33 -0500

    feat: estructuracion filtros

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,29 @@
 <template>
-  <div>
-    <center>
-      <h1>Bienvenidos a la app de tareas</h1>
-      <p>Esta es una aplicación que permite gestionar las tareas de los estudiantes</p>
-
-      <a href="/tareas">Click para iniciar (sin nuxtLink)</a>
-<br>
-<br>
-      <NuxtLink to="/tareas">
-        Click para iniciar (Con nuxtLink)
-      </NuxtLink>
-    </center>
-
-  </div>
+  <v-card>
+    <v-layout>
+      <v-navigation-drawer permanent theme="default">
+          <v-list nav>
+            <br><br><br>
+            <v-card-title class="card_title">Filtros</v-card-title>
+            <br>
+            <v-list-item-title>Categorias</v-list-item-title>
+            <v-list-item prepend-icon="mdi-email" title="Inbox" value="inbox"></v-list-item>
+            <v-list-item prepend-icon="mdi-account-supervisor-circle" title="Supervisors"
+            value="supervisors"></v-list-item>
+            <v-list-item-title>Marcas</v-list-item-title>
+            <v-list-item prepend-icon="mdi-email" title="Inbox" value="inbox"></v-list-item>
+            <v-list-item prepend-icon="mdi-account-supervisor-circle" title="Supervisors"
+            value="supervisors"></v-list-item>
+            <v-list-item-title>Precios</v-list-item-title>
+            <v-list-item prepend-icon="mdi-email" title="Inbox" value="inbox"></v-list-item>
+            <v-list-item prepend-icon="mdi-account-supervisor-circle" title="Supervisors"
+              value="supervisors"></v-list-item>
+            <v-list-item prepend-icon="mdi-clock-start" title="Clock-in" value="clockin"></v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+      <v-main style="height:700px"></v-main>
+    </v-layout>
+  </v-card>
 </template>
 <script>
 definePageMeta({
