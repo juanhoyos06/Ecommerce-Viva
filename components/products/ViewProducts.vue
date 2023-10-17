ac4622bec917ee432f54955714b77df2a08a8f69
juanhoyos06 <jphoyos0610@gmail.com>
2023-10-17 17:16:57 -0500

    feat: Visualizacion de productos

---
+++ components/products/ViewProducts.vue
@@ -0,0 +1,94 @@
+<template>
+    <v-item-group selected-class="bg-primary">
+        <v-container fluid>
+            <br><br><br>
+            <h1 class="text-center">Productos</h1>
+            <br>
+            <v-row>
+                <v-col v-for="product in products" :key="product.id" cols="cols" md="4">
+                    <v-item v-slot="{ selectedClass, toggle }">
+                        <v-card class="mx-auto pa-6" height="400" width="300" dark @click="openDialog(product)" color="black"
+                            elevation="18">
+
+                            <v-img :src="`./src/assets/img/products/${product.image}`" height="330" cover>
+                            </v-img>
+
+
+                            <v-card-title class="text-center">{{ product.name }}</v-card-title>
+                        </v-card>
+                    </v-item>
+                </v-col>
+            </v-row>
+            <v-dialog v-model="dialog" max-width="700px" max-height="400px">
+                <v-card style="border-radius: 20px">
+                    <v-card-title class="text-center"
+                        style="font-size: 30px; font-family: Times New Roman, serif; font-weight: bold; background-color: black; color: white;">
+                        {{ selectedProduct.name }}
+                    </v-card-title>
+                    <v-row no-gutters>
+                        <v-col cols="6">
+                            <v-img :src="product.image" height="auto" cover>
+                            </v-img>
+                        </v-col>
+                        <v-col cols="6" class="d-flex align-center">
+                            <v-row no-gutters>
+                                <v-card-subtitle>
+                                    Marca
+                                </v-card-subtitle>
+
+                                <v-card-text>
+                                    {{ selectedProduct.brand || 'No hay descripción para mostrar...' }}
+                                </v-card-text>
+                                <v-card-subtitle>
+                                    Categoria
+                                </v-card-subtitle>
+
+                                <v-card-text>
+                                    {{ selectedProduct.Category || 'No hay descripción para mostrar...' }}
+                                </v-card-text>
+
+                            </v-row>
+                        </v-col>
+                    </v-row>
+                </v-card>
+            </v-dialog>
+        </v-container>
+    </v-item-group>
+</template>
+<script setup>
+import axios from "axios";
+
+const products = ref([])
+const dialog = ref(false)
+const selectedProduct = ref({})
+
+onBeforeMount(() => {
+    loadTasks()
+})
+const loadTasks = async () => {
+    const url = 'http://localhost:3001/products'
+    const { data } = await axios.get(url)
+    console.log(data);
+    products.value = data
+}
+
+// const deleteTasks = async (item) => {
+//     const url = `http://localhost:3001/tasks/${item.id}`
+//     const { data } = await axios.delete(url)
+//     loadTasks()
+// }
+// const editTasks = async (item) => {
+//     isEdit.value = true
+//     editTask.value = { ... item}
+//     loadTasks()
+//     console.log(editTask.value);
+
+// }
+
+// const updateTask = (isUpdated) =>{
+//     console.log(isUpdated);
+//     isEdit.value = false
+//     editTask.value = null
+//     loadTasks()
+// }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,35 @@
 <template>
-  <v-card>
+  <v-card class="overflow-hidden">
     <v-layout>
-      <v-navigation-drawer permanent theme="default">
-          <v-list nav>
-            <br><br><br>
-            <v-card-title class="card_title">Filtros</v-card-title>
-            <br>
-            <v-list-item-title>Categorias</v-list-item-title>
-            <v-list-item prepend-icon="mdi-email" title="Inbox" value="inbox"></v-list-item>
-            <v-list-item prepend-icon="mdi-account-supervisor-circle" title="Supervisors"
-            value="supervisors"></v-list-item>
-            <v-list-item-title>Marcas</v-list-item-title>
-            <v-list-item prepend-icon="mdi-email" title="Inbox" value="inbox"></v-list-item>
-            <v-list-item prepend-icon="mdi-account-supervisor-circle" title="Supervisors"
-            value="supervisors"></v-list-item>
-            <v-list-item-title>Precios</v-list-item-title>
-            <v-list-item prepend-icon="mdi-email" title="Inbox" value="inbox"></v-list-item>
-            <v-list-item prepend-icon="mdi-account-supervisor-circle" title="Supervisors"
-              value="supervisors"></v-list-item>
-            <v-list-item prepend-icon="mdi-clock-start" title="Clock-in" value="clockin"></v-list-item>
-          </v-list>
-        </v-navigation-drawer>
-      <v-main style="height:700px"></v-main>
+      <v-row class="ma-0">
+        <v-col cols="3" class="pa-0">
+          <v-navigation-drawer permanent theme="default">
+            <v-list nav>
+              <br><br><br>
+              <v-card-title class="card_title">Filtros</v-card-title>
+              <br>
+              <v-list-item-title>Categorias</v-list-item-title>
+              <v-list-item prepend-icon="mdi-email" title="Inbox" value="inbox"></v-list-item>
+              <v-list-item prepend-icon="mdi-account-supervisor-circle" title="Supervisors"
+                value="supervisors"></v-list-item>
+              <v-list-item-title>Marcas</v-list-item-title>
+              <v-list-item prepend-icon="mdi-email" title="Inbox" value="inbox"></v-list-item>
+              <v-list-item prepend-icon="mdi-account-supervisor-circle" title="Supervisors"
+                value="supervisors"></v-list-item>
+              <v-list-item-title>Precios</v-list-item-title>
+              <v-list-item prepend-icon="mdi-email" title="Inbox" value="inbox"></v-list-item>
+              <v-list-item prepend-icon="mdi-account-supervisor-circle" title="Supervisors"
+                value="supervisors"></v-list-item>
+              <v-list-item prepend-icon="mdi-clock-start" title="Clock-in" value="clockin"></v-list-item>
+            </v-list>
+          </v-navigation-drawer>
+        </v-col>
+        <v-col cols="9" class="pa-0"> 
+          <v-main class="pa-0 overflow-y-auto" style="max-height: 100vh;">
+            <ProductsViewProducts />
+          </v-main>
+        </v-col>
+      </v-row>
     </v-layout>
   </v-card>
 </template>
@@ -29,4 +37,34 @@
 definePageMeta({
   layout: "default",
 });
+
+import axios from "axios";
+export default {
+  data() {
+    return {
+      dialog: false,
+      characters: [],
+      selectedCharacter: {}
+
+    }
+  },
+  mounted() {
+    axios.get('https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=699987288fb2dc8a3483835fc5d27322&hash=3df1e8d77029aa53d48fc11664f3a491&limit=100')
+      .then(response => {
+        this.characters = response.data.data.results.filter(hero => {
+          return hero.thumbnail.path != 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available';
+        });
+      })
+      .catch(error => {
+        console.error("Hubo un error", error);
+      });
+  },
+  methods: {
+    openDialog(item) {
+      this.selectedCharacter = item;  // Guarda el personaje seleccionado
+      this.dialog = true;  // Abre el v-dialog
+    }
+  }
+};
+
 </script>
