913b600b91b6c3d85095a8d7908204c63260a96b
JuanGomez-14 <jmgomezc14@gmail.com>
2023-10-19 00:42:14 -0500

    Feat: funcion editar usuario ya funciona

--- pages/editar-usuario.vue
+++ pages/editar-usuario.vue
@@ -21,17 +21,8 @@
           <v-card>
             <v-card-title class="headline">Editar Perfil</v-card-title>
             <v-card-text>
-              <v-text-field
-                v-model="name"
-                label=""
-                :value="currentUser.user ? currentUser.user.name : 'nombre: '"
-              ></v-text-field>
-
-              <v-text-field
-                v-model="email"
-                label=""
-                :value="currentUser.user ? currentUser.user.email : ''"
-              ></v-text-field>
+              <v-text-field v-model="name" label="Nombre"></v-text-field>
+              <v-text-field v-model="email" label="Correo"></v-text-field>
             </v-card-text>
             <v-card-actions>
               <v-spacer></v-spacer>
@@ -54,24 +45,44 @@ export default {
   data() {
     return {
       dialog: false,
-      name: "", // Agregar las variables para name y email
-      email: "",
+      id: currentUser.user ? currentUser.user.id : "",
+      name: currentUser.user ? currentUser.user.name : "",
+      email: currentUser.user ? currentUser.user.email : "",
+      password: currentUser.user ? currentUser.user.password : "",
     };
   },
   methods: {
-    async fetchUser() {
+    async guardarCambios() {
       try {
-        const response = await axios.get("http://localhost:3001/users");
-        //currentUser.setUser(response.data[0]);
+        // Preparar los datos para actualizar
+        const dataToUpdate = {
+          id: this.id,
+          name: this.name,
+          email: this.email,
+          password: this.password,
+        };
+
+        // Realizar la solicitud PUT a la base de datos (ajusta la URL según tu API)
+        await axios.put(
+          `http://localhost:3001/users/${currentUser.user.id}`,
+          dataToUpdate
+        );
+
+        // Actualizar el usuario en currentUser si es necesario
+        currentUser.setUser({
+          ...currentUser.user,
+          name: this.name,
+          email: this.email,
+        });
+
+        // Redirigir o mostrar un mensaje de éxito
+        this.$router.push("./");
       } catch (error) {
-        console.error("Error fetching user data", error);
+        console.error("Error al actualizar el usuario", error);
+        // Manejar errores o mostrar un mensaje de error al usuario
       }
     },
-    editar() {
-      this.dialog = false;
-    },
     logOut() {
-      // Implement your logout logic here
       currentUser.setUser(null);
       this.$router.push("/login");
     },
@@ -81,13 +92,8 @@ export default {
       return currentUser;
     },
   },
-  mounted() {
-    if (!currentUser.user) {
-      this.fetchUser();
-    }
+  definePageMeta: {
+    layout: "blank",
   },
 };
-definePageMeta({
-  layout: "blank",
-});
 </script>
